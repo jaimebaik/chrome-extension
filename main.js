0db334bd5f80477dfe4648a07b681d5a42d4e7cf
jaimebaik <jaimebaik5@gmail.com>
2020-10-07 22:35:43 -0700

    changed the gif to a image when loaded and stylized the fox news

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ let mainNode = document.querySelector("main");
 let APIdata;
 changePublisher();
 
+  let image = document.createElement('img');
+  let imageLink = "https://assets.teenvogue.com/photos/5dae2cb156a2b0000860086a/16:9/w_2560%2Cc_limit/Pol_Fake-News_10-21_PROMO.jpg";
+  image.setAttribute('src', imageLink);
+  image.setAttribute('id', 'news-gif');
+  //image.addEventListener('click', changeToRandom);
+  //change the style of image
+  image.setAttribute('style', "width: 100%");
+  mainNode.prepend(image);
+
 //API call and promises
 fetch('https://api.giphy.com/v1/gifs/search?api_key=FrywqnDisisLRkoqGAL4rYImXBKHJLFW&q=fake news&limit=100&offset=0&rating=g&lang=en')
 .then((data) => data.json())
@@ -11,14 +20,14 @@ fetch('https://api.giphy.com/v1/gifs/search?api_key=FrywqnDisisLRkoqGAL4rYImXBKH
   console.log(APIdata[0]['images']['original']['url']);
   let randomNum = Math.floor(Math.random()*APIdata.length);
   //create & prepend image
-  let image = document.createElement('img');
-  let imageLink = APIdata[randomNum]['images']['original']['url'];
-  image.setAttribute('src', imageLink);
-  image.setAttribute('id', 'news-gif');
+  let image = document.querySelector('#news-gif');
+  // let imageLink = APIdata[randomNum]['images']['original']['url'];
+  // image.setAttribute('src', imageLink);
+  // image.setAttribute('id', 'news-gif');
   image.addEventListener('click', changeToRandom);
-  //change the style of image
-  image.setAttribute('style', "width: 100%");
-  mainNode.prepend(image);
+  // //change the style of image
+  // image.setAttribute('style', "width: 100%");
+  // mainNode.prepend(image);
  
 })
 // .then(changePublisher);
@@ -52,7 +61,18 @@ function changePublisher(){
       publisher = article[i].children[2].children[0].children[2];
     }
     //change innerText inside
-    publisher.innerText += " Fake News"
+    if(!publisher.innerText.includes('Fox')){
+
+      publisher.innerText += " Fake News";
+    }
+    else{
+      publisher.style.color = "red";
+      publisher.style.fontWeight = "bold";
+      publisher.parentNode.parentNode.parentNode.style.border = "1px solid red";
+      publisher.parentNode.parentNode.parentNode.style.background = "url('https://d279m997dpfwgl.cloudfront.net/wp/2019/07/AP_19201004713022-1000x667.jpg') no-repeat top right";
+      publisher.parentNode.parentNode.parentNode.style.backgroundSize = "50px auto";
+
+    }
   }
   
 }
